f24d86361e39ba19d2daf2daf6346dc58c06a1c2
giflores1026 <flores.geraldivan@gmail.com>
2024-10-11 15:12:12 +0800

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('courses', CourseController::class)->except('update', 'destroy'); // can't edit, delete courses
+
+Route::apiResource('students', StudentController::class);
