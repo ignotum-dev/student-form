5d5bf5684c3ee7e25e6269295b5ec47e151c9b17
giflores1026 <flores.geraldivan@gmail.com>
2024-10-11 10:20:27 +0800

    Created the students table

---
+++ database/migrations/2024_10_11_003840_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Course;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_number');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->foreignIdFor(Course::class)->constrained()->onDelete('cascade'); //model class, cascade(course delete = user delete)
+            $table->enum('year', ['First Year', 'Second Year', 'Third Year', 'Fourth Year']);
+            $table->date('dob');
+            $table->unsignedTinyInteger('age');
+            $table->enum('sex', ['Male', 'Female']);
+            $table->string('c_address');
+            $table->string('h_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
