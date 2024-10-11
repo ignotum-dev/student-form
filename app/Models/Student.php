228a63eae609829a50e1a2fe318309b496b80b84
giflores1026 <flores.geraldivan@gmail.com>
2024-10-11 15:15:27 +0800

    Edited the fillable

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,20 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'student_number',
         'first_name',
         'middle_name',
         'last_name',
         'course_id', //foreign key
         'year',
         'dob',
+        'age',
         'sex',
         'c_address',
         'h_address',
